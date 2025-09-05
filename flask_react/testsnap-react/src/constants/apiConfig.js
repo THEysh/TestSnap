3589c81b76e2e4ebd4705df3d64cb93e4f47a818
theysh <1084701312@qq.com>
2025-09-05 22:04:34 +0800

    updata

--- flask_react/testsnap-react/src/constants/apiConfig.js
+++ flask_react/testsnap-react/src/constants/apiConfig.js
@@ -1,5 +1,5 @@
 // API配置文件
-export const API_BASE_URL = 'http://localhost:7861/api';
+export const API_BASE_URL = 'http://192.168.1.128:7861/api';
 export const ENDPOINTS = {
   PDF_UPLOAD: `${API_BASE_URL}/pdf/upload`,
   PDF_PROCESS: `${API_BASE_URL}/pdf/process`,
